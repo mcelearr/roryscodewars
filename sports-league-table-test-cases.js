26bd31e2ee82fcc636c180b9933fb07c7a1ac307
Rory McElearney <rorymcelearney@gmail.com>
2015-12-26 18:18:30 +0000

    Update sports-league-table-test-cases.js

--- sports-league-table-test-cases.js
+++ sports-league-table-test-cases.js
@@ -13,8 +13,26 @@ Test.assertSimilar(leagueCalculate("teamA","teamB","draw"), [["teamB",11],["team
 
 Test.describe("Random tests",function(){
 function randint(a,b){return Math.floor(Math.random()*(b-a+1)+a);}
-function leagueCalculate(team1, team2, result) {return leagueTable=leagueTable.map( ([n,s]) => [n,s+((n==team1)*((result=="win")*3+(result=="draw")) || n==team2 && result=="draw")] ).sort((a,b) => (b[1]-a[1])||(a[0]>b[0]) )}
+function leagueSolution(team1, team2, result) {return leagueSol=leagueSol.map( ([n,s]) => [n,s+((n==team1)*((result=="win")*3+(result=="draw")) || n==team2 && result=="draw")] ).sort((a,b) => (b[1]-a[1])||(a[0]>b[0]) )}
 var leagueTable = [["CodeWarriors",0],["Founders&Coders",0],["Quorans",0],["CodeCademists",0],["Eden of the East", 0],["Max Headroom", 0],["FSociety", 0],["Public Security Section 9", 0]]
 var leagueSol = [["CodeWarriors",0],["Founders&Coders",0],["Quorans",0],["CodeCademists",0],["Eden of the East", 0],["Max Headroom", 0],["FSociety", 0],["Public Security Section 9", 0]]
 var teams=["CodeWarriors", "Founders&Coders", "Quorans", "CodeCademists", "Eden of the East", "Max Headroom", "FSociety", "Public Security Section 9"]
 )}
+
+function shuffle (array) {
+  var i = 0
+  var j = 0
+  var temp = null
+
+  for (i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1))
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+}
+//loop in here
+  var players=shuffle(teams)
+  var match=[players[0], players[1], ["win","draw"][randint(0,1)]]
+  Test.assertSimilar(leagueCalculate(*([]+match)), leagueSolution(*match), "It should work for random inputs too")
+  Test.assert_equals(leagueTable, leagueSol, "It should work for random inputs too")
